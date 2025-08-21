6bf086933bfcf6670c782229c7888ca55bad91dd
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2025-08-22 00:54:03 +0700

    update exclude page

--- src/main/java/com/voc/config/SessionInterceptor.java
+++ src/main/java/com/voc/config/SessionInterceptor.java
@@ -22,7 +22,7 @@ public class SessionInterceptor implements HandlerInterceptor {
         // get current page URL
         String currentPage = request.getRequestURI();
 
-        String[] excludedPages = { "/home", "/login", "/register", "/css/**", "/js/**", "/resources/**", "/welcome" };
+        String[] excludedPages = { "/home", "/welcome", "/login", "/register", "/css/**", "/js/**", "/resources/**", "/welcome" };
 
         if (cookies != null) {
             for (Cookie cookie : cookies) {
