bb551c92a88fb31bc948c3831b976bf1d2b63150
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2025-08-26 04:14:23 +0700

    chore(clean): clean up unused import and java @tags

--- src/main/java/com/voc/api/AuthApiController.java
+++ src/main/java/com/voc/api/AuthApiController.java
@@ -8,15 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.voc.security.AuthManager;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/auth")
