995bbd2cb853e4c9e62b7658b79c2cbdaae7c138
Zartex <56326190+ZartexVertagen@users.noreply.github.com>
2025-08-26 14:56:01 +0700

    fix(conflict): revert back to original code

--- src/main/java/com/voc/config/WebConfig.java
+++ src/main/java/com/voc/config/WebConfig.java
@@ -11,6 +11,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(@NonNull InterceptorRegistry registry) {
         registry.addInterceptor(new SessionInterceptor())
                 .addPathPatterns("/**") // Check every page...
+<<<<<<< HEAD
                 .excludePathPatterns("/home", "/index", "/login", "/register", "/css/**", "/js/**", "/welcome", "/api/auth/**");
+=======
+                .excludePathPatterns("/home", "/index", "/login", "/register", "/css/**", "/js/**", "/welcome"); // ...except
+                                                                                                                 // these
+>>>>>>> parent of 46ec827 (feat: เพม about.jsp, contact.jsp และ PageController.java)
     }
 }
