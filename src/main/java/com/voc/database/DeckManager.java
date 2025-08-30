128cf444f410534a9d2f182563276d08f20b5f46
Petch Hongkaew <82324764+K-Hongkaew@users.noreply.github.com>
2025-08-31 03:55:28 +0700

    feat(deckManger): add func
    - add forkDeck
    -add deleteDeck
    -add updateDeck (WIP)

--- src/main/java/com/voc/database/DeckManager.java
+++ src/main/java/com/voc/database/DeckManager.java
@@ -47,28 +47,29 @@ public class DeckManager {
                 deckId != null ? deckId : Snowflake.nextId(), name, isPublic, userId);
     }
 
-    public static void createNewLevel(Long levelId, String levelName, int levelWeight, Long deckId){
+    public static void createNewLevel(Long levelId, String levelName, int levelWeight, Long deckId) {
         DatabaseUtils.sqlPrepareStatement(
                 "INSERT INTO card_leveltb (level_id_PK, level_name, level_weight, deck_id_FK) VALUES (?, ?, ?, ?)",
                 levelId != null ? levelId : Snowflake.nextId(), levelName, levelWeight, deckId);
     }
 
-    public static void createNewCard(Long cardId, Long levelId, String cardName){
+    public static void createNewCard(Long cardId, Long levelId, String cardName) {
         DatabaseUtils.sqlPrepareStatement(
                 "INSERT INTO cardtb (card_id_PK, level_id_FK, card_word) VALUES (?, ?, ?)",
                 cardId != null ? cardId : Snowflake.nextId(), levelId, cardName);
     }
 
-    public static void createNewPartOfSpeech(Long posID, Long cardId, String pos){
+    public static void createNewPartOfSpeech(Long posID, Long cardId, String pos) {
         DatabaseUtils.sqlPrepareStatement(
                 "INSERT INTO postb (pos_id_PK, card_id_FK, part_of_speech) VALUES (?, ?, ?)",
                 posID != null ? posID : Snowflake.nextId(), cardId, pos);
     }
-    
-    public static void createNewDefinition(Long definitionId, Long posID, String definition){
+
+    public static void createNewDefinition(Long definitionId, Long posID, String definition) {
         DatabaseUtils.sqlPrepareStatement(
                 "INSERT INTO definitiontb (definition_id_PK, pos_id_FK, definition) VALUES (?, ?, ?)",
-                definitionId != null ? definitionId : Snowflake.nextId(), posID, definition != null ? definition : "New Definition");
+                definitionId != null ? definitionId : Snowflake.nextId(), posID,
+                definition != null ? definition : "New Definition");
     }
 
     /**
@@ -78,7 +79,8 @@ public class DeckManager {
         long deckCount = ((Number) DatabaseUtils.sqlSingleRowStatement(
                 "SELECT COUNT(*) FROM decktb").get("COUNT(*)")).longValue();
 
-        if (deckCount != 0) return;
+        if (deckCount != 0)
+            return;
 
         Long rootUserID = ((Number) DatabaseUtils.sqlSingleRowStatement(
                 "SELECT user_id_PK FROM usertb WHERE username = ?", DatabaseUtils.getRootUsername())
@@ -89,11 +91,14 @@ public class DeckManager {
 
         ObjectMapper mapper = new ObjectMapper();
 
-        try (InputStream input = DeckManager.class.getClassLoader().getResourceAsStream("datasets/default_deck_sample.json")) {
-            if (input == null) throw new RuntimeException("File not found!");
+        try (InputStream input = DeckManager.class.getClassLoader()
+                .getResourceAsStream("datasets/default_deck_sample.json")) {
+            if (input == null)
+                throw new RuntimeException("File not found!");
 
             Map<String, Map<String, Map<String, Map<String, List<String>>>>> rootJson = mapper.readValue(input,
-                    new TypeReference<>() {});
+                    new TypeReference<>() {
+                    });
             Map<String, Map<String, Map<String, List<String>>>> defaultDeck = rootJson.get("default");
 
             List<Object[]> levelBatch = new ArrayList<>();
@@ -104,20 +109,20 @@ public class DeckManager {
             int weight = 1;
             for (Map.Entry<String, Map<String, Map<String, List<String>>>> levelData : defaultDeck.entrySet()) {
                 Long levelId = Snowflake.nextId();
-                levelBatch.add(new Object[]{levelId, weight, levelData.getKey(), deckId});
+                levelBatch.add(new Object[] { levelId, weight, levelData.getKey(), deckId });
                 weight++;
 
                 for (Map.Entry<String, Map<String, List<String>>> cardData : levelData.getValue().entrySet()) {
                     Long cardId = Snowflake.nextId();
-                    cardBatch.add(new Object[]{cardId, levelId, cardData.getKey()});
+                    cardBatch.add(new Object[] { cardId, levelId, cardData.getKey() });
 
                     for (Map.Entry<String, List<String>> posData : cardData.getValue().entrySet()) {
                         Long posId = Snowflake.nextId();
-                        posBatch.add(new Object[]{posId, cardId, posData.getKey()});
+                        posBatch.add(new Object[] { posId, cardId, posData.getKey() });
 
                         for (String def : posData.getValue()) {
                             Long defId = Snowflake.nextId();
-                            defBatch.add(new Object[]{defId, posId, def});
+                            defBatch.add(new Object[] { defId, posId, def });
                         }
                     }
                 }
@@ -125,7 +130,8 @@ public class DeckManager {
 
             // Execute batches
             DatabaseUtils.sqlExecuteBatch(
-                    "INSERT INTO card_leveltb (level_id_PK, level_weight, level_name, deck_id_FK) VALUES (?, ?, ?, ?)", levelBatch);
+                    "INSERT INTO card_leveltb (level_id_PK, level_weight, level_name, deck_id_FK) VALUES (?, ?, ?, ?)",
+                    levelBatch);
             DatabaseUtils.sqlExecuteBatch(
                     "INSERT INTO cardtb (card_id_PK, level_id_FK, card_word) VALUES (?, ?, ?)", cardBatch);
             DatabaseUtils.sqlExecuteBatch(
@@ -144,5 +150,43 @@ public class DeckManager {
 
         System.out.println(deckList);
         return deckList.getData();
+    }
+
+    // if targetDeck doesn't in forktb
+    // add deck_id to forktb first
+    // otherwise add user_id
+    public static String forkDeck(long deck_id, long user_id) {
+        SQLResult deckFork = DatabaseUtils
+                .sqlPrepareStatement("SELECT * FROM forktb WHERE deck_id_FK = ? AND user_id_FK = ?", deck_id, user_id);
+
+        if (deckFork.getData().isEmpty()) {
+            DatabaseUtils.sqlPrepareStatement("INSERT INTO forktb (deck_id_FK, user_id_FK) VALUES (?, ?)", deck_id,
+                    user_id);
+            return "Success";
+        }
+        // not sure there anything more
+        return "Cannot fork the deck";
+    }
+
+    public static String deleteDeck(long deck_id, long user_id) {
+        Row deleteDeck = DatabaseUtils.sqlSingleRowStatement(
+                "SELECT * FROM decktb WHERE deck_id_PK = ? AND user_id_FK = ?", deck_id, user_id);
+
+        if (deleteDeck != null) {
+            DatabaseUtils.sqlSingleRowStatement("DELETE FROM decktb WHERE deck_id_PK = ? AND user_id_FK = ?", deck_id,
+                    user_id);
+        }
+        // multiple deck delete ????
+        return "Delete success";
+    }
+
+    // UPDATE table_name SET A1 = ?, A2 =? WHERE <condition>
+    // can set card
+    // can set deck
+    // change level?
+    // WIP
+    public static String updateDeck(long deck_id, long card_id) {
+        SQLResult updateDeck = DatabaseUtils.sqlPrepareStatement("SELECT * FROM  ");
+        return "yes";
     }
 }
