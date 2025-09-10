3cd076070e8c4b0588e22aff5b553be8273ffce0
Petch Hongkaew <82324764+K-Hongkaew@users.noreply.github.com>
2025-09-10 20:34:38 +0700

    feat(getCards): api for get cards from decks

--- src/main/java/com/voc/api/ReviewApiController.java
+++ src/main/java/com/voc/api/ReviewApiController.java
@@ -1,6 +1,8 @@
 package com.voc.api;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.voc.database.DeckManager;
+import com.voc.utils.Row;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -27,8 +33,21 @@ public class ReviewApiController {
      * @return
      */
     @GetMapping("/getCards")
-    public ResponseEntity<Map<String, Object>> reviewCard(@RequestParam ArrayList<Long> arrayDeckId) {
-        
+    public static ResponseEntity<Map<String, Object>> reviewCard(@RequestParam ArrayList<Long> arrayDeckId) {
+        Map<String, Object> response = new HashMap<>();
+        // It's still an array
+        if (arrayDeckId != null) {
+            List<Object> decks = new ArrayList<>();
+            for (int i = 0; i < arrayDeckId.size(); i++) {
+                List<Row> cards = DeckManager.getAllCardFromDeck(arrayDeckId.get(i));
+                decks.add(cards);
+            }
+            response.put("Cards", decks);
+            return ResponseEntity.ok(response);
+        } else {
+            System.out.println("No deck_id has send");
+        }
+        System.out.println(arrayDeckId);
         return null;
     }
 }
