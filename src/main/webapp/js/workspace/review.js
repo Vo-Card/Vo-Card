a3866ca3b4fba85692ba4ec05f152357ef4b3394
Petch Hongkaew <82324764+K-Hongkaew@users.noreply.github.com>
2025-09-19 23:29:05 +0700

    chore: remove debugger and put cardStage out of loop

--- src/main/webapp/js/workspace/review.js
+++ src/main/webapp/js/workspace/review.js
@@ -32,12 +32,9 @@ export async function sendPackDeckId(ids) {
 }
 
 async function randomizer(usrDecks) {
-
     // starting randomization
     const whichDeck = Math.floor(Math.random() * usrDecks.length)
-    console.log(usrDecks[whichDeck]);
     const whichCard = Math.floor(Math.random() * usrDecks[whichDeck].length)
-    console.log(usrDecks[whichDeck][whichCard]);
 
     return usrDecks[whichDeck][whichCard];
     // cardStage.textContent(packArray.Cards[whichDeck][whichCard]);
@@ -56,22 +53,21 @@ export async function cardSessionPlay(sessionPlay) {
     const usrCards = await sendPackDeckId(["752111449966383104"]);
 
     for (let i = 0; i < sessionPlay; i++) {
-        console.log("IT'S STARTED");
 
         let whatCard = await randomizer(usrCards);
-        console.log(whatCard);
+        const cardStage = document.getElementById('card-placeholder');
 
         try {
             const cardDefinition = await fetchWithAuth(`/api/review/getDefinition?levelId=${whatCard.level_id_PK}&deckId=${whatCard.deck_id_FK}&cardId=${whatCard.card_id_PK}`);
             const data = await cardDefinition.json();
 
-            console.log(data);
-            const cardStage = document.getElementById('card-placeholder');
             populateContainer([whatCard], cardStage, "card", null);
 
         } catch (err) {
             console.error(err);
         }
+
+        
     }
 }
 
