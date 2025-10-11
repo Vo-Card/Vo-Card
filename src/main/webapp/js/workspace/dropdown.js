7340c7077c27e96ceb1c99a0db556d98bdac0a38
seinaru <82324764+K-Hongkaew@users.noreply.github.com>
2025-10-12 00:12:57 +0700

    feat: assign role
    - dropdown now loaded with permission name
    - on selected will update user to that role except ROOT

--- src/main/webapp/js/workspace/dropdown.js
+++ src/main/webapp/js/workspace/dropdown.js
@@ -30,27 +30,94 @@ export async function rtDropdowns() {
     });
 }
 
-let pageCache = 1;
-export async function assignRole(path) {
-    console.log("Ello");
-    const res = await fetchWithAuth("/api/user/getUser?page=" + pageCache);
+export async function assignRole() {
+    let pageCache = 1;
+    let person = 0;
 
-    if (res.ok) {
+    const nextButton = document.querySelector('.next');
+    const previousButton = document.querySelector('.previous');
+
+    nextButton.addEventListener('click', () => {
+        pageCache++;
+        
+    });
+    
+    previousButton.addEventListener('click', () => {
+        pageCache--;
+        if (pageCache <= 0){
+            pageCache = 1;
+        }
+    });
+
+    const res = await fetchWithAuth("/api/user/getUser?page=" + pageCache);
+    const resRole = await fetchWithAuth("/api/user/viewRoles")
+
+    if (res.ok && resRole.ok) {
         const data = await res.json();
+        const dataRole = await resRole.json();
         const container = document.querySelector("list-item");
         const roleItem = document.querySelector("role-item");
+        
         const roleItemCopy = roleItem.cloneNode(true);
+        const counter = document.querySelector('.all-user');
         roleItem.remove();
-        console.log(roleItemCopy);
-        console.log(data.User);
+
         data.User.forEach((user) => {
             const roleItemAppend = roleItemCopy.cloneNode(true);
             // @ts-ignore
             const username = roleItemAppend.querySelector(".username-display");
+            //@ts-ignore
+            const select = roleItemAppend.querySelector("selected-role");
+            //@ts-ignore
+            const selected = roleItemAppend.querySelector(".selected");
+            selected.textContent = user.permission_name;
+            //@ts-ignore
+            const roleContainer = roleItemAppend.querySelector(".role-container");
+            dataRole.Roles.forEach(role => {
+                const li = document.createElement('li');
+                li.textContent = role.permission_name;
+                li.setAttribute("permissionName", role.permission_name);
+                li.setAttribute("permissionId", role.permission_id_PK);
+                roleContainer.append(li);
+            });
 
+            //@ts-ignore
+            const roleItem = roleItemAppend.querySelectorAll('.role-container li');
+            select.addEventListener('click', () => {
+                select.classList.toggle("select-clicked");
+                roleContainer.classList.toggle("menu-open");
+            });
+
+            roleItem.forEach((role) => {
+                role.addEventListener('click', async () => {
+                    const uid = user.user_id_PK;
+                    const rid = role.getAttribute("permissionId");
+                    const preparedData = {
+                        targetId : uid,
+                        roleId : rid
+                    };
+                    
+                    const response = await fetchWithAuth("/api/user/assignRole", {
+                        method: "PUT",
+                        headers: { "Content-Type": "application/json"},
+                        body: JSON.stringify(preparedData),
+                    });
+
+                    if (response.ok){
+                        selected.textContent = role.getAttribute("permissionName");
+                        const chk = await response.json();
+                        console.log(chk);
+                    } else {
+                    }
+
+                })  
+            });
             username.textContent = user.username;
             container.append(roleItemAppend);
+            person++;
         });
+            //@ts-ignore
+        counter.textContent = person;
     } else {
         console.log("ello");
     }
